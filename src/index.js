e8dc51e90ef39afdddd20ed26f5d6b1931d940ae
Дмитрий Якорев <urakend@mail.ru>
2019-10-19 20:19:16 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (i = 0; i < str.length+3; i++){
+        for (j = 0; j < bracketsConfig.length; j++){
+            let temp = bracketsConfig[j].join('');
+            let indicator = str.includes(temp);
+        //  console.log('New =',indicator,temp);
+          if (indicator){
+            str = str.replace(temp, '');
+            console.log(str);
+          } ;
+        };
+      };
+      return str == "";
+    }
+
